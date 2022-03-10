127cddb73119abc205b7e81fa5378496a6b47736
Aduda-Boaz <adudaboaz@gmail.com>
2022-03-11 00:49:03 +0300

    Add fix linter errors

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -10,7 +10,8 @@ const BookForm = () => {
   const dispatch = useDispatch();
   const submitBookToStore = (e) => {
     e.preventDefault();
-    if (e.target.parentNode.lastChild[0].value && e.target.parentNode.lastChild[1].value && e.target.parentNode.lastChild[2].value) {
+    if (e.target.parentNode.lastChild[0].value && e.target.parentNode.lastChild[1].value
+      && e.target.parentNode.lastChild[2].value) {
       const newBook = {
         id: uuidv4(),
         title: e.target.parentNode.lastChild[0].value,
@@ -28,22 +29,22 @@ const BookForm = () => {
       <h3 className="add-new">Add newbook</h3>
       <form className="form-container" onSubmit={submitBookToStore}>
         <input
-            type="text"
-            placeholder="Book title"
-            name="title"
-            value={books.title}
-          />
-          <input
-            type="text"
-            placeholder="Book author"
-            name="author"
-            value={books.author}
-          />
-          <select placeholder="Category" name="category">
-            {categories.map((category) => (
-              <option key={uuidv4()} value={category}>{category}</option>
-            ))}
-          </select>
+          type="text"
+          placeholder="Book title"
+          name="title"
+          value={books.title}
+        />
+        <input
+          type="text"
+          placeholder="Book author"
+          name="author"
+          value={books.author}
+        />
+        <select placeholder="Category" name="category">
+          {categories.map((category) => (
+            <option key={uuidv4()} value={category}>{category}</option>
+          ))}
+        </select>
         <input type="submit" className="submit" value="add-book" />
       </form>
     </>
